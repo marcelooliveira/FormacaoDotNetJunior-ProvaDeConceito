e03f90d0dd93a76045abb4ae9d0514e55a874e48
marcelooliveira <mclricardo@gmail.com>
2018-04-24 17:32:10 -0300

    Ex3 - início

---
+++ Exercicios/Ex3/CasaDoCodigo/Migrations/20180423172554_Categorias.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace CasaDoCodigo.Migrations
+{
+    public partial class Categorias : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SubcategoriaId",
+                table: "Produto",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Cadastro",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.CreateTable(
+                name: "Categoria",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categoria", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Subcategoria",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CategoriaId = table.Column<int>(nullable: false),
+                    Nome = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subcategoria", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Subcategoria_Categoria_CategoriaId",
+                        column: x => x.CategoriaId,
+                        principalTable: "Categoria",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produto_SubcategoriaId",
+                table: "Produto",
+                column: "SubcategoriaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subcategoria_CategoriaId",
+                table: "Subcategoria",
+                column: "CategoriaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Produto_Subcategoria_SubcategoriaId",
+                table: "Produto",
+                column: "SubcategoriaId",
+                principalTable: "Subcategoria",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Produto_Subcategoria_SubcategoriaId",
+                table: "Produto");
+
+            migrationBuilder.DropTable(
+                name: "Subcategoria");
+
+            migrationBuilder.DropTable(
+                name: "Categoria");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Produto_SubcategoriaId",
+                table: "Produto");
+
+            migrationBuilder.DropColumn(
+                name: "SubcategoriaId",
+                table: "Produto");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Cadastro",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 50);
+        }
+    }
+}
---
+++ Exercicios/Ex3/CasaDoCodigo/Repositories/ProdutoRepository.cs
@@ -0,0 +1,58 @@
+﻿using CasaDoCodigo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Repositories
+{
+    public interface IProdutoRepository
+    {
+        void SaveProdutos(List<Livro> livros);
+        IList<Produto> GetProdutos();
+    }
+
+    public class ProdutoRepository : BaseRepository<Produto>, IProdutoRepository
+    {
+        private readonly ICategoriaRepository categoriaRepository;
+
+        public ProdutoRepository(ApplicationContext contexto,
+            ICategoriaRepository categoriaRepository) : base(contexto)
+        {
+            this.categoriaRepository = categoriaRepository;
+        }
+
+        public IList<Produto> GetProdutos()
+        {
+            return dbSet
+                .Include(prod => prod.Subcategoria)
+                .ThenInclude(sub => sub.Categoria)
+            .ToList();
+        }
+
+        public void SaveProdutos(List<Livro> livros)
+        {
+            foreach (var livro in livros)
+            {
+                var categoria = categoriaRepository.AddCategoria(livro.Categoria);
+                var subcategoria = categoriaRepository.AddSubcategoria(livro.Categoria, livro.Subcategoria);
+
+                if (!dbSet.Where(p => p.Codigo == livro.Codigo).Any())
+                {
+                    dbSet.Add(new Produto(livro.Codigo, livro.Nome, livro.Preco, subcategoria));
+                }
+            }
+            contexto.SaveChanges();
+        }
+    }
+
+    public class Livro
+    {
+        public string Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Categoria { get; set; }
+        public string Subcategoria { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
